284f8423b4b9785308fbfa497466828447952904
Luis Vega <vegaluisjose@gmail.com>
2020-11-15 04:35:47 +0000

    update

--- eval/python/compiler_perf.py
+++ eval/python/compiler_perf.py
@@ -27,19 +27,19 @@ def bench(name, out_dir, lengths):
         tadd(bench_name, l, reticle_file)
         for b in backends:
             print("Running {} backend with length={}".format(b, l))
-            use_dsp = True if b == "baseopt" else False
             file_name = "{}_{}".format(bench_name, b)
             verilog_file = create_path(out_dir, "{}.v".format(file_name))
-            reticle_asm_file = create_path(out_dir, "{}.rasm".format(file_name))
-            reticle_asm_placed_file = create_path(
-                out_dir, "{}_placed.rasm".format(file_name)
-            )
             if b == "reticle":
+                asm_file = create_path(out_dir, "{}.rasm".format(file_name))
+                placed_file = create_path(
+                    out_dir, "{}_placed.rasm".format(file_name)
+                )
                 start = perf_counter()
-                reticle_to_asm(reticle_file, reticle_asm_file)
-                reticle_place_asm(reticle_asm_file, reticle_asm_placed_file)
+                reticle_to_asm(reticle_file, asm_file)
+                reticle_place_asm(asm_file, placed_file)
                 elapsed = perf_counter() - start
             else:
+                use_dsp = True if b == "baseopt" else False
                 compile_baseline(reticle_file, verilog_file, use_dsp)
                 start = perf_counter()
                 vivado("synth_place.tcl", [out_dir, file_name, bench_name])
